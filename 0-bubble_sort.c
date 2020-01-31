9f9d95cda8e9a4c8b5efb0f1c786b9c263f06081
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-31 00:26:07 +0000

    Setting up repo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort - function that that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: Array of integers
+ * @size: Size_t variable for size of integer array
+ * Return: Void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a README for 0x1B. C - Sorting algorithms & Big O
