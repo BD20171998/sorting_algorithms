f06e552fcd4fbb53989be28297ffb53ab6625ace
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-31 02:19:01 +0000

    adding bubble sort function and updated header file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,5 +11,30 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp;
+
+	if (size == 1)
+	{
+		print_array(array, size);
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] <= array[j + 1])
+				continue;
+
+			else
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 
 }
