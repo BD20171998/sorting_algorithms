072b6200aef9477dfbc77cb6603615a86395fbfd
Bob Deprizio <robert.deprizio@gmail.com>
2020-02-03 22:54:11 +0000

    Updated bubble sort function for other edge cases

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,11 +14,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (size == 1)
-	{
-		print_array(array, size);
+	if (array == NULL || size < 2)
 		return;
-	}
 
 	for (i = 0; i < size - 1; i++)
 	{
