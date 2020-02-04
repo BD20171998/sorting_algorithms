b78cbec2583b60576f761ae6147d3dee0fd46746
Nehal Shastri <nhlshstr@gmail.com>
2020-02-03 18:44:00 -0800

    correct code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *before, *dot;
 
-	if (!list|| !*list|| !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 
 	curr = (*list)->next;
@@ -16,24 +16,20 @@ void insertion_sort_list(listint_t **list)
 	for (; curr; curr = curr->next)
 	{
 		before = curr;
-		printf("curr->n: %d\n", curr->n);
-		while (curr->n < before->prev->n && before && before->prev)
-		{	
+		while (before && before->prev && curr->n < before->prev->n)
+		{
 			dot = before->prev;
-			printf("dot->n: %d\n", dot->n);
-			printf("before->n: %d\n", dot->n);
 			if (dot->prev)
 				dot->prev->next = before;
 			/**Re-assign head*/
 			else
 				*list = before;
-			if (before->next != NULL)
+			if (before->next)
 				before->next->prev  = dot;
 			dot->next = before->next;
 			before->prev = dot->prev;
 			before->next = dot;
 			dot->prev = before;
-
 			print_list(*list);
 		}
 	}
